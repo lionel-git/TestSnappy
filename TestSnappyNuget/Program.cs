c2073d485eea9abb5cd40ae140bf718d0bf6aa6a
lionel@lionel-pc2 <lionel.desorme@free.fr>
2021-04-11 11:14:28 +0200

    Test nuget

---
+++ TestSnappyNuget/Program.cs
@@ -0,0 +1,22 @@
+﻿using Snappy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSnappyNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.OpenWrite("mydata.sz");
+            var compressor = new SnappyStream(file, CompressionMode.Compress);
+            var writer = new StreamWriter(compressor);
+            writer.WriteLine("Hello World!");
+        }
+    }
+}
